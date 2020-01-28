692c9f4612c7be147b7c3bb6d0ea74c8fed53f70
lucaslopes <lucaslopesledok@gmail.com>
2020-01-27 22:22:45 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,12 +4,12 @@ public class Board {
 
 	private int row;
 	private int columns;
-	private Piece[][] piece;
+	private Piece[][] pieces;
 
 	public Board(int row, int columns) {
 		this.row = row;
 		this.columns = columns;
-		piece = new Piece[row][columns];
+		pieces = new Piece[row][columns];
 	}
 
 	public int getRow() {
@@ -29,11 +29,16 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int column) {
-		return piece[row][column];
+		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
-		return piece[position.getRow()][position.getColumn()];
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;	
 	}
 	
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,11 +3,11 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPeice extends Piece {
+public class ChessPiece extends Piece {
 
 	private Color color;
 
-	public ChessPeice(Board board, Color color) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
